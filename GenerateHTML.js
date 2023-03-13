ceefec1268a7aeda75fe47981be2973dffaa9624
xMaedayx <120540407+xMaedayx@users.noreply.github.com>
2023-03-12 22:48:39 -0700

    updates

--- GenerateHTML.js
+++ GenerateHTML.js
@@ -3,6 +3,23 @@
 
 const addManager = function (manager) {
     return `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>TeamPage</title>
+        <link rel="stylesheet" href="./style.css" />
+        <link rel="stylesheet" href="./foundation.css">
+        
+      </head>
+      <body>
+        <header>
+            <nav class="is-active" id="is-active">
+                <span class="is-active" id="navigation-text">Team Profile</span>
+            </nav>
+        </header>
     <div class="row small-up-1 medium-up-2 large-up-3">
         <div class="column" style="flex: 0 0 auto;">
             <div class="card"> 
